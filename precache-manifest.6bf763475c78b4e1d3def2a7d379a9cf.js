ba1c7b9331cf5009d91a16d55a66c727eac7fb6a
Rizwan Mahmood <rizwanmahmood255@gmail.com>
2020-05-12 18:24:22 +0100

    Updates

---
+++ precache-manifest.6bf763475c78b4e1d3def2a7d379a9cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab698b93bceef2494631bf3ec304c9b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c37879693cd7a4d584b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c18b872476ed28cbfe1",
+    "url": "/static/js/2.389ded5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.389ded5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c37879693cd7a4d584b",
+    "url": "/static/js/main.380af27b.chunk.js"
+  },
+  {
+    "revision": "70a986ce62670b3d4794",
+    "url": "/static/js/runtime-main.dd2d9a55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
