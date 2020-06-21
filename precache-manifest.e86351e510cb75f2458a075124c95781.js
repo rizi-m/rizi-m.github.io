a384fa2932d0f1e1d211a958474ed466238602b3
Rizwan Mahmood <rizwan@pop-os.localdomain>
2020-06-21 14:13:33 +0100

    Updates

---
+++ precache-manifest.e86351e510cb75f2458a075124c95781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da828c8596b7885d5eaee467cd62c201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38811510a07a73800c91",
+    "url": "/static/css/main.0a89893a.chunk.css"
+  },
+  {
+    "revision": "61081512036d71b0296c",
+    "url": "/static/js/2.ae7958ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ae7958ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38811510a07a73800c91",
+    "url": "/static/js/main.c1b3e1ff.chunk.js"
+  },
+  {
+    "revision": "70a986ce62670b3d4794",
+    "url": "/static/js/runtime-main.dd2d9a55.js"
+  }
+]);
